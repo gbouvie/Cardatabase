79e3191e4e90b247cb651ed6621599056064e94b
gbouv <gbouv@192.168.0.14>
2021-10-24 23:05:30 -0300

    Insert data with foreign key relationship between Car and Owner

--- cardatabase/src/main/resources/import.sql
+++ cardatabase/src/main/resources/import.sql
@@ -1,3 +1,6 @@
-insert into car (id, brand, model, color, register_number, year, price) values(1, "Ford", "Mustang", "Red", "ADF-1121", 2017, 59000);
-insert into car (id, brand, model, color, register_number, year, price) values(2, "Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000);
-insert into car (id, brand, model, color, register_number, year, price) values(3, "Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000);
+insert into owner (ownerid, firstname, lastname) values(1, "John" , "Johnson");
+insert into owner (ownerid, firstname, lastname) values(2, "Mary" , "Robinson");
+
+insert into car (id, brand, model, color, register_number, year, price, owner) values(1, "Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, 1);
+insert into car (id, brand, model, color, register_number, year, price, owner) values(2, "Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, 2);
+insert into car (id, brand, model, color, register_number, year, price, owner) values(3, "Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, 2);
